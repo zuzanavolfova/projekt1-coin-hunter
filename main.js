9a78bb7b9e2585ee4ac69806efc8a78f3b42d056
Zuzana Volfová <zuzka.volfova@gmail.com>
2022-03-07 14:51:53 +0100

    let rename

--- main.js
+++ main.js
@@ -11,39 +11,44 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 let panacek = document.querySelector('#panacek')
 let mince = document.querySelector('#mince')
 
-let x = 500;
-let y = 400;
+let panacekX = 500;
+let panacekY = 400;
 
-let a = 100;
-let b = 200;
+let minceX = 100;
+let minceY = 200;
 
-panacek.style.left = x + 'px'
-panacek.style.top = y + 'px'
-mince.style.left = a + 'px'
-mince.style.top = b + 'px'
+panacek.style.left = panacekX + 'px'
+panacek.style.top = panacekY + 'px'
+mince.style.left = minceX + 'px'
+mince.style.top = minceY + 'px'
 
 
 function pohyb(event, velikostPohybu) {
     if (event.keyCode === 40) {
-        x = x;
-        y = y + velikostPohybu;
-        panacek.style.left = x + 'px';
-        panacek.style.top = y + 'px';
+        panacekX = panacekX;
+        panacekY = panacekY + velikostPohybu;
+        panacek.style.left = panacekX + 'px';
+        panacek.style.top = panacekY + 'px';
     } else if (event.keyCode === 38) {
-        x = x;
-        y = y - velikostPohybu;
-        panacek.style.left = x + 'px';
-        panacek.style.top = y + 'px';
+        panacekX = panacekX;
+        panacekY = panacekY - velikostPohybu;
+        panacek.style.left = panacekX + 'px';
+        panacek.style.top = panacekY + 'px';
     }
     if (event.keyCode === 37) {
-        x = x - velikostPohybu;
-        y = y;
-        panacek.style.left = x + 'px';
-        panacek.style.top = y + 'px';
+        panacekX = panacekX - velikostPohybu;
+        panacekY = panacekY;
+        panacek.style.left = panacekX + 'px';
+        panacek.style.top = panacekY + 'px';
     } else if (event.keyCode === 39) {
-        x = x + velikostPohybu;
-        y = y;
-        panacek.style.left = x + 'px';
-        panacek.style.top = y + 'px';
+        panacekX = panacekX + velikostPohybu;
+        panacekY = panacekY;
+        panacek.style.left = panacekX + 'px';
+        panacek.style.top = panacekY + 'px';
     }
+}
+
+if (!(panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || panacekY + panacekVyska < minceY || minceY + minceVyska < panacekY)) {
+    /* přičíst body
+    /*přesunout minci*/
 }
