0c0dc836b4ce79ee428e8a8cca733bafd3c6620e
Zuzana Volfová <zuzka.volfova@gmail.com>
2022-03-07 13:21:36 +0100

    add function pohyb

--- main.js
+++ main.js
@@ -9,7 +9,39 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 // sem začni psát svůj program
 
 let panacek = document.querySelector('#panacek')
-let x = 500
-let y = 400
+
+let x = 500;
+let y = 400;
+
 panacek.style.left = x + 'px'
 panacek.style.top = y + 'px'
+
+
+
+function pohyb(elementSelector, velikostPohybu) {
+    /*zjisti key code*/
+    if (keyCode === 40) {
+        x = x;
+        y = y + velikostPohybu;
+        elementSelector.style.left = x + 'px';
+        elementSelector.style.top = y + 'px';
+    } else if (keyCode === 38) {
+        x = x;
+        y = y - velikostPohybu;
+        elementSelector.style.left = x + 'px';
+        elementSelector.style.top = y + 'px';
+    }
+    if (keyCode === 37) {
+        x = x - velikostPohybu;
+        y = y;
+        elementSelector.style.left = x + 'px';
+        elementSelector.style.top = y + 'px';
+    } else if (keyCode === 39) {
+        x = x + velikostPohybu;
+        y = y;
+        elementSelector.style.left = x + 'px';
+        elementSelector.style.top = y + 'px';
+    }
+
+
+}
