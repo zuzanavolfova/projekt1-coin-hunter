a8d5950cbd36e7e910bbf515e9714780ba21df13
Zuzana Volfová <zuzka.volfova@gmail.com>
2022-03-09 13:13:41 +0100

    final function collectCoin

--- main.js
+++ main.js
@@ -15,9 +15,6 @@ let mince = document.querySelector('#mince')
 let panacekX = 500;
 let panacekY = 400;
 
-let minceX = 100;
-let minceY = 200;
-
 let maxX = window.screen.availWidth;
 let maxY = window.screen.availHeight;
 
@@ -35,8 +32,11 @@ let width = window.innerWidth;*/
 
 panacek.style.left = panacekX + 'px'
 panacek.style.top = panacekY + 'px'
-mince.style.left = Math.floor(Math.random() * (height - 1)) + 'px'
-mince.style.top = Math.floor(Math.random() * (width - 1)) + 'px'
+
+let minceX = Math.floor(Math.random() * (maxX - 1));
+let minceY = Math.floor(Math.random() * (maxY - 1));
+mince.style.left = minceX + 'px';
+mince.style.top = minceY + 'px';
 
 /*osetrit, aby panacek nevylezl z herniho planu*/
 
@@ -48,20 +48,25 @@ function movePanacek(a, b, c, d) {
 }
 
 function collectCoin() {
+
+
     if (!(panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || panacekY + panacekVyska < minceY || minceY + minceVyska < panacekY)) {
         console.log('jsem tu');
 
         scoreVypocet += 1;
         document.querySelector('#score').textContent = scoreVypocet;
-        mince.style.left = Math.floor(Math.random() * (width - 1)) + 'px';
-        mince.style.top = Math.floor(Math.random() * (height - 1)) + 'px';
+        minceX = Math.floor(Math.random() * (maxX - 1));
+        minceY = Math.floor(Math.random() * (maxY - 1));
+        mince.style.left = minceX + 'px';
+        mince.style.top = minceY + 'px';
         if (scoreVypocet === 6) {
             alert('Gratuluji. Posbíral jsi celý poklad.')
         }
     }
+    console.log(minceX, panacekX)
 }
 
-console.log(parseInt)
+
 
 function move(event, velikostPohybu) {
     if (panacekX < maxX && panacekX > 0 && panacekY < maxY && panacekY > 0) {
@@ -70,37 +75,36 @@ function move(event, velikostPohybu) {
             movePanacek(panacekX, panacekY + velikostPohybu, panacekX + 'px', panacekY + 'px');
             panacek.src = 'obrazky/panacek.png';
 
-            collectCoin()
 
         } else if (event.keyCode === 38) {
             /*nahoru*/
             movePanacek(panacekX, panacekY - velikostPohybu, panacekX + 'px', panacekY + 'px');;
             panacek.src = 'obrazky/panacek-nahoru.png';
-            collectCoin()
+
         } else if (event.keyCode === 37) {
             /*doleva*/
             movePanacek(panacekX - velikostPohybu, panacekY, panacekX + 'px', panacekY + 'px');
             panacek.src = 'obrazky/panacek-vlevo.png';
-            collectCoin()
+
         } else if (event.keyCode === 39) {
             /*doprava*/
             movePanacek(panacekX + velikostPohybu, panacekY, panacekX + 'px', panacekY + 'px');
             panacek.src = 'obrazky/panacek-vpravo.png';
-            collectCoin()
         }
     }
     /*omezeni pohybu na okno*/
     else if (panacekX >= maxX) {
         movePanacek(panacekX - 5, panacekY, panacekX + 'px', panacekY + 'px')
         panacek.src = 'obrazky/panacek.png';
-        collectCoin()
+        c
     } else if (panacekY >= maxY) {
         movePanacek(panacekX, panacekY - 5, panacekX + 'px', panacekY + 'px')
         panacek.src = 'obrazky/panacek.png';
-        collectCoin()
+
     } else {
         movePanacek(panacekX + 5, panacekY + 5, panacekX + 'px', panacekY + 'px')
         panacek.src = 'obrazky/panacek.png';
-        collectCoin()
+
     }
+    collectCoin();
 }
